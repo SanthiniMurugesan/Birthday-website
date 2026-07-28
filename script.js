4c15ff4f269e18eebedc187bc718e6bf50c457b6
Santhini Murugesan <santhini0525@gmail.com>
2026-07-29 00:14:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    const surprise = document.getElementById("surprise");
+
+    if (surprise.style.display === "block") {
+        surprise.style.display = "none";
+    } else {
+        surprise.style.display = "block";
+    }
+}
